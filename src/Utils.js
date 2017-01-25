87bb2795f2351ad5517d6cec084ba4a439d81497
Flascher <alexanderflasch@gmail.com>
2017-01-24 22:20:36 -0600

    Fixed error in api structure
    Also moved Crystalys over to using axios,
    Removed core-js as its unneeded now.

--- src/Utils.js
+++ src/Utils.js
@@ -1,5 +1,4 @@
 var chalk = require('chalk');
-var core = require('core-js/library');
 
 var apiKey = '';
 
@@ -52,31 +51,33 @@ module.exports = {
     },
 
 	generatePromise(url) {
-		console.log(url.uri);
+		// console.log(url.uri);
 
-		return new Promise((resolve, reject) => {
-			const lib = core.String.startsWith(url.uri, 'https') ? require('https') : require('http');
+		// return new Promise((resolve, reject) => {
+		// 	const lib = core.String.startsWith(url.uri, 'https') ? require('https') : require('http');
+		//
+		// 	const options = url.uri;
+		//
+    	// 	const request = lib.request(url, (response) => {
+		// 		// handle http errors
+		// 	    if (response.statusCode < 200 || response.statusCode > 299) {
+		// 	    	reject(new Error('Failed to load page, status code: ' + response.statusCode));
+		// 	    }
+		//
+		// 		// temporary data holder
+		// 		const body = [];
+		//
+		// 		// on every content chunk, push it to the data array
+		// 		response.on('data', (chunk) => body.push(chunk));
+		//
+		// 		// we are done, resolve promise with those joined chunks
+		// 		response.on('end', () => resolve(body.join('')));
+		//     });
+		//
+		//     // handle connection errors of the request
+		//     request.on('error', (err) => reject(err))
+	    // });
 
-			const options = url.uri;
-
-    		const request = lib.request(url, (response) => {
-				// handle http errors
-			    if (response.statusCode < 200 || response.statusCode > 299) {
-			    	reject(new Error('Failed to load page, status code: ' + response.statusCode));
-			    }
-
-				// temporary data holder
-				const body = [];
-
-				// on every content chunk, push it to the data array
-				response.on('data', (chunk) => body.push(chunk));
-
-				// we are done, resolve promise with those joined chunks
-				response.on('end', () => resolve(body.join('')));
-		    });
-
-		    // handle connection errors of the request
-		    request.on('error', (err) => reject(err))
-	    });
+		return axios.get(url);
 	}
 };
